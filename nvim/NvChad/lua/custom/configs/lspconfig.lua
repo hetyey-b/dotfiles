5c32ac6ecc841b803ec71e0dfa75e7d91b0c54ec
hetyey-b <hetyey.botond@gmail.com>
2023-06-01 13:24:51 +0200

    Fixed tmux navigator shortcuts

--- nvim/NvChad/lua/custom/configs/lspconfig.lua
+++ nvim/NvChad/lua/custom/configs/lspconfig.lua
@@ -3,20 +3,19 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require("lspconfig")
 
--- conflicts with debugger
--- lspconfig.rust_analyzer.setup({
---   on_attach = on_attach,
---   capabilities = capabilities,
---   filetypes = {"rust"},
---   root_dir = lspconfig.util.root_pattern("Cargo.toml"),
---   settings = {
---     ['rust-analyzer'] = {
---       cargo = {
---         allFeatures = true,
---       },
---     },
---   },
--- })
+lspconfig.rust_analyzer.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"rust"},
+  root_dir = lspconfig.util.root_pattern("Cargo.toml"),
+  settings = {
+    ['rust-analyzer'] = {
+      cargo = {
+        allFeatures = true,
+      },
+    },
+  },
+})
 
 lspconfig.eslint.setup({
   on_attach = function(client, bufnr)
--- nvim/NvChad/lua/custom/plugins.lua
+++ nvim/NvChad/lua/custom/plugins.lua
@@ -27,20 +27,6 @@ local plugins = {
       vim.g.rustfmt_autosave = 1
     end
   },
-  {
-    "simrat39/rust-tools.nvim",
-    ft = "rust",
-    dependencies = "neovim/nvim-lspconfig",
-    opts = function()
-      return require "custom.configs.rust-tools"
-    end,
-    config = function(_, opts)
-      require('rust-tools').setup(opts)
-    end
-  },
-  {
-    "mfussenegger/nvim-dap"
-  }
 }
 
 return plugins
