29109e1a1bc31f1c088b7af5e32a7527a72ac63b
hetyey-b <hetyey.botond@gmail.com>
2023-08-22 11:34:50 +0200

    Added relative line numbers

--- nvim/NvChad/lua/custom/init.lua
+++ nvim/NvChad/lua/custom/init.lua
@@ -8,6 +8,8 @@ vim.opt.hlsearch = false
 vim.opt.scrolloff = 12
 vim.opt.wrap = false
 
+vim.opt.relativenumber = true
+
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
